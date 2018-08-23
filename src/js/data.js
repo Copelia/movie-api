5ac6fd6aec33e9b6c31683c4c5802c4e1ff41981
[Copelia] <iamcorallium@gmail.com>
2018-08-22 19:12:43 -0500

    Cubriendo requisitos de primer alcance

--- src/js/data.js
+++ src/js/data.js
@@ -1,15 +1,26 @@
-// let searchQuery = document.getElementById('input').value;
-//  Paso de convertir espacios a +
-// const url = ('http://www.omdbapi.com/?apikey=2a4884f1&s=`{searchQuery}`&plot=short&type=movie')
+// const url = ('http://www.omdbapi.com/?apikey=2a4884f1&s=The+Lord+Of+The+Rings&plot=short&type=movie');
 
-const url = ('http://www.omdbapi.com/?apikey=2a4884f1&s=The+Lord+Of+The+Rings&plot=short&type=movie');
+let api = '2a4884f1';
+let search;
+// Función para opción de select con alguna de las 3 películas
+const selected = () => {
+  let movieSelect = document.getElementById('select');
+  search = movieSelect.options[movieSelect.selectedIndex].value;
+  //  console.log(search);
+  getMovies(search);
+};
+// Llamando a función selected con evento en etiqueta select
+const selectChange = () => {
+  selected();
+};
 
-
-window.onload = () => {
+window.getMovies = (search) => {
+  url = 'http://www.omdbapi.com/?apikey='+ api + '&s='+ search + '&plot=short&type=movie';  
   fetch(url)
     .then(response => (response.json())
       .then((data) => {
-        getMovies(data);
+        console.log(data);
+        getArray(data);
       })
       .catch((error) => {
         console.log('Error: ', error);
--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,17 @@
+
 let newMoviesArr = [];
 
-window.getMovies = (data) => {
-  //  Obteniendo arreglo de objetos con los resultados de la api para un título :c
+window.getArray = (data) => {
+//  Obteniendo arreglo de objetos con los resultados de la api para un título :c
   let results = data.Search;
-  //  console.log(results);
-
+  console.log(results);
+  
   // Recorriendo el arreglo y recolecando un valor de cada objeto de acuerdo a la propiedad señalada
   let moviesArr = results.map((element) => {
     let titles = element.Title;
     let year = element.Year;
     let catalogueID = element.imdbID;
-    console.log(titles, year, catalogueID);
+    // console.log(titles, year, catalogueID);
 
     return {
       títulos: element.Title,
@@ -18,27 +19,26 @@ window.getMovies = (data) => {
       identificador: catalogueID
     };
   });
-  // Arreglo de objetos
+    // Arreglo de objetos
   newMoviesArr.push(moviesArr);
   console.log(newMoviesArr);
 
   return newMoviesArr;
 };
 
-
 // Imprime undefined T_T
-const printMovies = (array) =>{
-  let button = document.getElementById('btn-movies');
-  let container = document.getElementById('results');
-  let printResult = '' ;
-  button.addEventListener('click', event =>{
-    for (i = 0; i < newMoviesArr.length; i++) {
-      printResult += `<ul><p>Título</p>
-      <li>${newMoviesArr.títulos}</li> 
-      </ul>`;
-      console.log(newMoviesArr.títulos);
-      container.innerHTML = printResult;
-    }
-  });
-};
-printMovies();
+// const printMovies = (array) =>{
+//   let button = document.getElementById('btn-movies');
+//   let container = document.getElementById('results');
+//   let printResult = '' ;
+//   button.addEventListener('click', event =>{
+//     for (i = 0; i < newMoviesArr.length; i++) {
+//       printResult += `<ul><p>Título</p>
+//       <li>${newMoviesArr.títulos}</li> 
+//       </ul>`;
+//       console.log(newMoviesArr.títulos);
+//       container.innerHTML = printResult;
+//     }
+//   });
+// };
+// printMovies();
